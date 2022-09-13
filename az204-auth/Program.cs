1611967e2cb62eb80f1b6c5742d52c6fd5b68ccc
Del Maguey Techonologies <dan13lpj@hotmail.com>
2022-09-13 17:30:29 -0500

    MSAL Authorization sample

---
+++ az204-auth/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace az204_auth
+{
+    class Program
+    {
+        private const string _clientId = "<ClientID>";
+        private const string _tenantId = "<TenantID>";
+
+        public static async Task Main(string[] args)
+        {
+            IPublicClientApplication app = PublicClientApplicationBuilder
+                        .Create(_clientId)
+                        .WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+                        .WithRedirectUri("http://localhost")
+                        .Build();
+
+            string[] scopes = { "user.read" };
+            AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+            Console.WriteLine($"Token:\t{result.AccessToken}");
+
+
+        }
+    }
+}
